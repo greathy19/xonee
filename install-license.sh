9c3523726936bc7843af8a3d89b4789a3037a853
Greathy <157180527+greathy19@users.noreply.github.com>
2026-02-26 07:00:21 -0500

    Update and rename install-crack.sh to install-license.sh

--- install-license.sh
+++ install-license.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ ! -f "/etc/systemd/system/xuione.service" ] && echo "XUI.one isn't installed!" && exit
 [ ! -d "/home/xui/config" ] && echo "XUI.one isn't installed!" && exit
-echo "XUI.one Crack"
+echo "XUI.one license"
 echo "-------------"
 echo "All Versions"
 echo "By sysnull84"
@@ -10,7 +10,7 @@ echo "-------------
 echo "Stopping XUI.one
 "
 sudo systemctl stop xuione
-echo "Installing cracked license
+echo "Installing license
 "
 cp -r license /home/xui/config/license
 cp -r xui.so /home/xui/bin/php/lib/php/extensions/no-debug-non-zts-20190902/xui.so
@@ -20,5 +20,5 @@ sed -i "s/^license.*/license     =   \"cracked\"/g" /home/xui/config/config.ini
 echo "Starting XUI.one
 "
 sudo systemctl start xuione
-echo "Cracked! ;)
+echo "listo! ;)
 "
